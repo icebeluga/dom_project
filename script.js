04cb4e9ece3e2f349b5141afefac2e1e1ded6994
icebeluga <gbjohnnyliu2009@gamil.com>
2024-08-12 17:12:49 -0400

    First COmmit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 // Add your code to this file
+let back_color= document.querySelector("body"); 
+
+back_color.style.backgroundColor = "#00008b";
+
+let burder= document.querySelectorAll("img");
+for(let i =0; i<burder.length; i++){
+    burder[i].style.border = "4px dotted #a6854c";
+}
+
+let names= document.getElementsByClassName("pokemon_names");
+for(let i =0; i<names.length; i++){
+    names[i].style.color = "#e0dea1";
+    names[i].innerHTML += "!";
+}
+let position= document.querySelector("h2"); 
+
+position.style.textAlign = "center";
+
+let Bulbasaur= document.getElementById("bulbasaur_container"); 
+
+Bulbasaur.style.backgroundColor = "#388e8e";
+
+let rewording= document.querySelector("footer"); 
+
+rewording.style.borderBottom= "2px solid white";
+rewording.innerHTML = "All Star Code | DOM Project";
+
+
+document.querySelector()
+document.getElementById()
+document.querySelectorAll()
+document.getElementsByClassName()
+
